b2efa7279d396a17421f795f44cb7a19c1ed7d26
magway <magway@ecwid.com>
2021-11-19 16:56:36 +0200

    02-01 sort strings

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
+  const arrCopy = [...arr];
+  const direction = param === 'desc' ? -1 : 1;
+  const locales = ['ru', 'en'];
 
+  return arrCopy.sort( (string1, string2) => {
+    return direction * string1.localeCompare(string2, locales, { caseFirst: 'upper' });
+  });
 }
